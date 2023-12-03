d1f59c84c5039ee3657c8aa7505d7eeec08b2710
laura <e12119041@student.tuwien.ac.at>
2023-12-03 15:26:52 +0100

    Add camera ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CameraComponent } from './camera/camera.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    //redirectTo: 'folder/Inbox',
+    redirectTo: 'camera',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'camera',
+    children: [
+      { path: '', component: CameraComponent }
+    ] 
   }
 ];
 
---
+++ src/app/camera/camera.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-camera',
+  templateUrl: './camera.component.html',
+  styleUrls: ['./camera.component.scss'],
+})
+export class CameraComponent  implements OnInit {
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+    let i = 0
+  }
+
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+  }
+
+}
